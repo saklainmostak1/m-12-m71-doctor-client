ce76caf8d29c6d2a920aa888d42b53275e66dc0a
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-12 10:43:23 +0600

    card

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Card from '../Card/Card';
 import InfoCards from '../InfoCards/InfoCards';
 import Services from '../Services/Services';
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <InfoCards></InfoCards>
             <Services></Services>
+            <Card></Card>
         </div>
     );
 };
