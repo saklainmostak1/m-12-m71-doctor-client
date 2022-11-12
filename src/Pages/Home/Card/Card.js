dc91cac75c7cac528b02a7e1328eb9de0159481e
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-12 12:07:52 +0600

    started btn

--- src/Pages/Home/Card/Card.js
+++ src/Pages/Home/Card/Card.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import treatment from '../../../assets/images/treatment.png'
+import PrimaryBtn from '../../../components/PrimaryBtn/PrimaryBtn';
 
 const Card = () => {
     return (
-        <div className="hero mt-5">
+        <div className="hero mt-20">
   <div className="hero-content flex-col lg:flex-row">
     <img src={treatment} alt='' className="lg:w-1/2 rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+      <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+      <PrimaryBtn>Geting Started</PrimaryBtn>
     </div>
   </div>
 </div>
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Card from '../Card/Card';
 import InfoCards from '../InfoCards/InfoCards';
+import MakeAppointment from '../Make Appointment/MakeAppointment';
 import Services from '../Services/Services';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <InfoCards></InfoCards>
             <Services></Services>
             <Card></Card>
+            <MakeAppointment></MakeAppointment>
         </div>
     );
 };
