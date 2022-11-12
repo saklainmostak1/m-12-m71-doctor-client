36772f408071ef6e2b739dc96d7607fc2ecebbff
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-13 02:38:40 +0600

    json

--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
+import AviableAbleAppiontment from '../AviableAbleAppiontment/AviableAbleAppiontment';
 
 const Appointment = () => {
+    const [selectedDate , setSelectedDate] = useState(new Date())
     return (
         <div>
-            <AppointmentBanner></AppointmentBanner>
+            <AppointmentBanner 
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            ></AppointmentBanner>
+            <AviableAbleAppiontment
+            selectedDate={selectedDate}
+            ></AviableAbleAppiontment>
         </div>
     );
 };
--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import chair from '../../../assets/images/chair.png'
 import { DayPicker } from 'react-day-picker';
-import { format } from 'date-fns';
 
-const AppointmentBanner = () => {
-    const [selectedDate , setSelectedDate] = useState(new Date())
+
+const AppointmentBanner = ({selectedDate, setSelectedDate}) => {
+    
     return (
         <header className='my-6'>
             <div className="hero ">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='Dentist chair' />
+                    <img src={chair} className="max-w-sm lg:w-1/2 rounded-lg shadow-2xl" alt='Dentist chair' />
                     <div className='mr-6'>
                         <DayPicker
                         mode='single'
                         selected={selectedDate}
                         onSelect={setSelectedDate}
                         ></DayPicker>
-                        <p>You have selected date: {format(selectedDate, 'PP' )} </p>
                     </div>
                 </div>
             </div>
